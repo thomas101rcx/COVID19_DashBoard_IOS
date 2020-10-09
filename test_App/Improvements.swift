19350bcb4564ac773d83deb1bb6611197fc84010
thomas101rcx <tl164@duke.edu>
2020-10-09 23:07:32 +0800

    10092020Commit

--- test_App/Improvements.swift
+++ test_App/Improvements.swift
@@ -10,6 +10,9 @@
 // 1. Change the URL download from github section to only once instead of doing it in every view controller // Done
 // 1-1 apply MVC to code and separate the functions part with the view loaders // Done
 // 2. Rename some of the variables and simplify the code  //Done
-// 3. Implement background activity to prefetch stored location's covid info
+// 3. Implement background activity to prefetch stored location's covid info // In progress
+// 3-1 Added refresh mode so when the app is open, the stored location's covid 19 data is up to date // Done
+// 3-2 Add background activity to send local notification to user every day for their stored location's covid 19 data // In progress
 // 4. Add ranking // Done
+
 
